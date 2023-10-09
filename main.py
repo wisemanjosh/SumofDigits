98f5adcd4843326cfabba2167c9e226f862e6c09
JosueLessie <joslessie@hotmail.com>
2023-10-09 21:26:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Sum of digits
+number = 47
+
+# Extract the tens and ones of digits
+tens_digit = number // 10
+ones_digit = number % 10
+
+# Calculate the sum of digits
+sum_of_digits = tens_digit + ones_digit
+
+print("Sum of digits:", sum_of_digits)
